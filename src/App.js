43a219a1bc870b210f17b17c88ecde03028f9e26
Adrian Menzel <adrian.menzel@voltstorage.com>
2018-01-10 19:42:33 +0100

    first skeleton

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">ModEX</h1>
+            <h4 className="App-subtitle">Your modular index</h4>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
